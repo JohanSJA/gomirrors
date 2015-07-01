9a4e7a419c78bfcabefaa494d4d05c75adacdbc5
Johan Sim Jian An <johansja@gmail.com>
2015-07-01 09:07:52 +0800

    Sort by rate.

--- gomirrors.go
+++ gomirrors.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
+	"sort"
+	"time"
 )
 
 type MirrorURL struct {
@@ -19,6 +23,12 @@ type MirrorURL struct {
 	DurationAvg    float64 `json:"duration_avg"`
 }
 
+type ByAge []MirrorURL
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].LastSync < a[j].LastSync }
+
 type MirrorStatus struct {
 	Cutoff         int         `json:"cutoff"`
 	CheckFrequency int         `json:"check_frequency"`
@@ -27,6 +37,17 @@ type MirrorStatus struct {
 	Version        int         `json:"version"`
 	URLs           []MirrorURL `json:"urls"`
 }
+
+type MirrorRate struct {
+	URL  MirrorURL
+	Rate float64
+}
+
+type ByRate []MirrorRate
+
+func (r ByRate) Len() int           { return len(r) }
+func (r ByRate) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r ByRate) Less(i, j int) bool { return r[i].Rate < r[j].Rate }
 
 func Mirrors() (MirrorStatus, error) {
 	var mirror MirrorStatus
@@ -45,6 +66,55 @@ func Mirrors() (MirrorStatus, error) {
 	return mirror, nil
 }
 
+func Rate(mirror MirrorURL) MirrorRate {
+	mr := MirrorRate{
+		URL:  mirror,
+		Rate: math.MaxFloat64,
+	}
+
+	url := mirror.URL + "core/os/i686/core.db"
+	start := time.Now()
+	resp, err := http.Get(url)
+	end := time.Now()
+	if err != nil {
+		return mr
+	}
+	defer resp.Body.Close()
+
+	dur := end.Sub(start).Seconds()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return mr
+	}
+	size := len(body)
+
+	rate := float64(size) / dur
+
+	return MirrorRate{
+		URL:  mirror,
+		Rate: rate,
+	}
+}
+
+func Rates(mirrors []MirrorURL) []MirrorRate {
+	mr := make(chan MirrorRate)
+
+	for _, m := range mirrors {
+		go func(mu MirrorURL, mr chan MirrorRate) {
+			mr <- Rate(mu)
+		}(m, mr)
+	}
+
+	var rates []MirrorRate
+	for range mirrors {
+		r := <-mr
+		rates = append(rates, r)
+	}
+
+	return rates
+}
+
 func main() {
 	log.Print("gomirrors started")
 
@@ -54,4 +124,14 @@ func main() {
 	}
 
 	log.Printf("Mirror Status: %+v", ms)
+
+	sort.Sort(sort.Reverse(ByAge(ms.URLs)))
+	mirrors := ms.URLs[:200]
+	log.Println()
+	log.Printf("200 Latest Sync Mirror: %+v", mirrors)
+
+	rates := Rates(mirrors)
+	sort.Sort(sort.Reverse(ByRate(rates)))
+	log.Println()
+	log.Printf("Mirror Rates: %+v", rates)
 }
